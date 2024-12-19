1fee9928e259e02e98ebdd41832cd17164f13476
dattatadepalli <datta.t@talentsprint.com>
2024-12-19 14:47:44 +0530

    Update ErrorPage.jsx

--- src/react/ErrorPage.jsx
+++ src/react/ErrorPage.jsx
@@ -42,7 +42,7 @@ function ErrorPage({
             <p className="text-muted testing">
               <FormattedMessage
                 id="unexpected.error.message.text"
-                defaultMessage="An unexpected error occurred. Please click the button below to refresh the page to continue"
+                defaultMessage="An unexpected error occurred. Please click the button below to refresh the page"
                 description="error message when an unexpected error occurs"
               />
             </p>
